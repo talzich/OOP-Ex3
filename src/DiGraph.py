9200592baf7539688696554a7cf93b6c441c4a09
Tal Zichlinsky <talzhtc@gmail.com>
2021-01-05 17:43:32 +0200

    Implemented Digraph

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -6,7 +6,7 @@ from Edge import Edge
 class DiGraph(GraphInterface):
 
     def __init__(self):
-        self.nodes = dict()
+        self.__nodes = dict()
         self.__edge_size = 0
         self.__node_size = 0
         self.__mc = 0
@@ -21,11 +21,11 @@ class DiGraph(GraphInterface):
         return self.__mc
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        if id1 not in self.nodes or id2 not in self.nodes:
+        if id1 not in self.__nodes or id2 not in self.__nodes:
             return False
 
-        src = self.nodes[id1]
-        dest = self.nodes[id2]
+        src = self.__nodes[id1]
+        dest = self.__nodes[id2]
         src_outs = src.get_out()
 
         if id1 == id2:
@@ -43,25 +43,39 @@ class DiGraph(GraphInterface):
             return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
-        if node_id not in self.nodes:
+        if node_id not in self.__nodes:
             new_node = Node(node_id, pos)
-            self.nodes[node_id] = new_node
+            self.__nodes[node_id] = new_node
             self.__node_size += 1
             self.__mc += 1
             return True
         return False
 
     def remove_node(self, node_id: int) -> bool:
-        pass
+        if node_id not in self.__nodes:
+            return False
+
+        node = self.__nodes[node_id]
+        out_keys = list(node.get_out().keys())
+        in_keys = list(node.get_in().keys())
+
+        for key in out_keys:
+            self.remove_edge(node_id, key)
+        for key in in_keys:
+            self.remove_edge(key, node_id)
+
+        self.__nodes.pop(node_id)
+        self.__mc += 1
+        self.__node_size -= 1
+        return True
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        if node_id1 not in self.nodes or node_id2 not in self.nodes:
+        if node_id1 not in self.__nodes or node_id2 not in self.__nodes:
             return False
-        src = self.nodes[node_id1]
-        dest = self.nodes[node_id2]
+        src = self.__nodes[node_id1]
+        dest = self.__nodes[node_id2]
 
         src_outs = src.get_out()
-        dest_ins = dest.get_in()
 
         if node_id2 not in src_outs:
             return False
@@ -75,3 +89,12 @@ class DiGraph(GraphInterface):
     def __str__(self):
         return f'Node Size: {self.__node_size}, Edge Size: {self.__edge_size} mc: {self.__mc}'
 
+
+graph = DiGraph()
+graph.add_node(0)
+graph.add_node(1)
+graph.add_edge(0, 1, 1)
+print(graph.remove_node(0))
+print(graph.remove_node(1))
+print(graph.remove_node(1))
+print(graph.add_node(0))
